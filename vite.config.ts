5663e217816404dc956d039002445157f518187b
Guillaume Adam <guillaume@gadam.fr>
2025-04-14 14:20:42 +0200

    build: fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 3000
+    port: 3000,
+    host: '0.0.0.0', 
+    allowedHosts: true,
   },
   build: {
     outDir: 'dist',
